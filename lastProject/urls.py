9c675cea790579598118bed812a722f1e7fbf1fb
Hadeer Nabil <Hadeer@Hadeers-MacBook-Pro.local>
2014-05-19 23:03:17 +0200

    urls

--- lastProject/urls.py
+++ lastProject/urls.py
@@ -8,7 +8,8 @@ urlpatterns = patterns('',
     # url(r'^$', 'lastProject.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', 'myapp.views.view'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^update/(?P<place>\w{0,30})/(?P<direction>\w{0,30})/(?P<statue>\w{0,30})/$', 'myapp.views.update'),
-    url(r'^view/$', 'myapp.views.view'),
+    
 )
