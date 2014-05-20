79d75ce59c6432102ba35397e4bbf207a05eb5d8
Hadeer Nabil <Hadeer@Hadeers-MacBook-Pro.local>
2014-05-20 18:12:59 +0200

    views.py

--- myapp/views.py
+++ myapp/views.py
@@ -1,19 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from myapp.models import *
 
 # Create your views here.
 def update(request,place,direction,statue):
-	p=Places.objects.get(name=place,direction=direction)
-	p.crowded = statue
-	p.save()
-	places = Places.objects.all()
-	return render(request,
-	              'ITS.html',
-	              {'places':places})
+	try:
+		p=Places.objects.get(name=place,direction=direction)
+		p.crowded = statue
+		p.save()
+		places = Places.objects.all()
+		return HttpResponse('DONE')
+	except Exception, e:
+		return HttpResponse(e)
+
 
 def view(request):
 	places = Places.objects.all()
 	return render(request,
 	              'ITS.html',
 	              {'places':places})
-
